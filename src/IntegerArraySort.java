76436069c23b277db4e03d3775ecaaddaf65dd6d
William Shi <williamshi5358@gmail.com>
2024-04-07 01:42:00 -0700

    created a new branch of iteration0, and added IntegerArraySort.java

---
+++ src/IntegerArraySort.java
@@ -0,0 +1,21 @@
+
+
+import java.io.*;
+//student name: Weijun Shi
+//Id:100408663
+//INFO4190
+
+public class IntegerArraySort{  
+    public static void insertionSort(int array[]) {  
+        int n = array.length;  
+        for (int j = 1; j < n; j++) {  
+            int key = array[j];  
+            int i = j-1;  
+            while ( (i > -1) && ( array [i] > key ) ) {  
+                array [i+1] = array [i];  
+                i--;  
+            }  
+            array[i+1] = key;  
+        }  
+    }
+}
